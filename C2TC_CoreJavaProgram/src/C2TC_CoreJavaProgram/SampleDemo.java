5334b0b6d6a02cee87754c88f46c1684a9d7534d
STUDENT <STUDENT@DESKTOP-PJSUP61>
2025-07-21 16:30:04 +0530

    Day 1 -----> Java core programs

--- C2TC_CoreJavaProgram/src/C2TC_CoreJavaProgram/SampleDemo.java
+++ C2TC_CoreJavaProgram/src/C2TC_CoreJavaProgram/SampleDemo.java
@@ -1,11 +1,29 @@
 package C2TC_CoreJavaProgram;
 
 public class SampleDemo {
-
+	public static void main() {
+		System.out.println("Welcome from main 1");
+		
+	}
+	public static void main(char c[]) {
+		System.out.println("Welcome from main 2");
+		
+	}
+	public static void main(String ar) {
+		System.out.println("Welcome from main 3");
+		
+	}
+		
+	static {
+		System.out.println("Static block 2");
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Day1");
 		System.out.println("hello");
-
+		
 	}
-
+	
+	static {
+		System.out.println("Static block 1");
+	}
 }
