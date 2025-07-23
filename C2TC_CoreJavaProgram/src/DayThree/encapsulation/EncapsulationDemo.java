fc3c264c58ed0c19d9693d63dcda7741de8872e4
AbineshwarV <abineshwarv01@gmail.com>
2025-07-23 21:03:20 +0530

    Day Three Assignment

---
+++ C2TC_CoreJavaProgram/src/DayThree/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// object creation
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Abineshwar V");
+		obj.setAge(21);
+		System.out.println(obj);
+
+	}
+
+}
