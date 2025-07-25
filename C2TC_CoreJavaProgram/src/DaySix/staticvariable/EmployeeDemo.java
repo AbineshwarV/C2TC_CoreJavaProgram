9791718c8faa4dd526478c2c1a86565585665bcb
Abineshwar V <abineshwarv01@gmail.com>
2025-07-25 15:44:51 +0530

    Day Six Assignment

---
+++ C2TC_CoreJavaProgram/src/DaySix/staticvariable/EmployeeDemo.java
@@ -0,0 +1,19 @@
+package DaySix.staticvariable;
+
+//Program to demonstrate static variables
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		// Create the first object of the class and pass the two arguments with type
+		// string and int.
+		System.out.println(Employee.companyName);
+		Employee e = new Employee("AbineshwarV", 123);
+		System.out.println(e);
+
+		// Similarly, create the second object of the class and pass the two arguments.
+		e = new Employee("AdhiV", 321);
+		System.out.println(e);
+
+	}
+
+}
